59c14848014481c000d1d62563ed679f142c4168
Jules <jules.laviolette@outlook.fr>
2023-08-21 06:16:23 +0200

    hook admin, css commande

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -24,3 +24,11 @@ function child_enqueue_styles() {
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+function add_extra_item_to_nav_menu( $items, $args ) {
+    if (is_user_logged_in() && $args->theme_location == "primary" || "") {
+        $items .= '<li><a id="bouton-admin" href="'. get_permalink( get_option('') ) .'">Admin</a></li>';
+    }
+
+    return $items;
+}
